f103088522c583f6ce52427e68e9c211e31e2566
PresentKim <debe3721@gmail.com>
2024-06-20 15:32:56 +0900

    feat(ajax.js): Form 기반 AJAX 요청과 일반 AJAX 요청을 분리

--- src/main/webapp/static/script/ajax.js
+++ src/main/webapp/static/script/ajax.js
@@ -1,44 +1,26 @@
 var lastAjaxRequest = null;
 
 /**
- * AJAX 요청을 전송하는 함수
+ * XMLHttpRequest 객체를 사용하여 POST 요청을 전송하는 함수
+ *
+ * @param xhr - XMLHttpRequest 객체
+ * @param requestBody - POST 요청 바디
+ * @param ajaxHandler - 응답 처리 함수
  */
-function ajax(requestUrl, requestBody, ajaxHandler) {
-    // 마지막 요청 취소
+function sendHttpRequest(xhr, requestBody, ajaxHandler) {
+    // 마지막 요청 취소 후 갱신
     if (lastAjaxRequest) {
         lastAjaxRequest.abort();
     }
+    lastAjaxRequest = xhr;
 
-    // requestUrl이 유효하지 않으면 `location.pathname`를 사용
-    if (!requestUrl) {
-        requestUrl = location.pathname;
-    }
-
-    // ajaxHandler이 유효하지 않으면 `defaultAjaxHandler`를 사용
+    // ajaxHandler 값이 유효하지 않으면 `defaultAjaxHandler`를 사용
     if (!ajaxHandler) {
         ajaxHandler = defaultAjaxHandler;
     }
 
-    // requestBody가 리터럴 객체인 경우 문자열로 변환
-    if (typeof requestBody === 'object') {
-        requestBody = Object
-            .entries(requestBody)
-            .filter(([, value]) => value !== null && value !== undefined)
-            .map(([key, value]) => `${key}=${value}`).join('&');
-    }
-
-    // requestUrl로 POST 요청 객체 생성
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", requestUrl, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
     // POST 응답 이벤트 핸들러 등록
-    xhr.onreadystatechange = function () {
-        // 완료 전까지 무시
-        if (xhr.readyState !== 4) {
-            return;
-        }
-
+    xhr.onload = function () {
         // abort()로 취소된 요청은 무시
         if (xhr.status === 0) {
             return;
@@ -48,16 +30,40 @@ function ajax(requestUrl, requestBody, ajaxHandler) {
         ajaxHandler(xhr.status, JSON.parse(xhr.responseText));
     };
 
-    // requestBody로 POST 요청 전송
+    // requestBody 그대로 POST 요청 전송
     xhr.send(requestBody);
-    lastAjaxRequest = xhr;
 }
 
 /**
- * HTML Form 요소를 AJAX 요청으로 전송하는 함수
- * onSubmit 이벤트 핸들러로 사용
+ * AJAX 요청을 전송하는 함수 (requestBody 값이 객체인 경우 쿼리스트링으로 변환)
+ *
+ * @param {string} requestUrl - 요청 URL
+ * @param {Object} requestBody - 요청 바디 객체
+ * @param {function} ajaxHandler - 응답 처리 함수
  */
-function ajaxSubmit(event) {
+function ajax(requestUrl, requestBody, ajaxHandler) {
+    // requestBody 값이 리터럴 객체인 경우 문자열로 변환
+    if (typeof requestBody === 'object') {
+        requestBody = Object
+            .entries(requestBody)
+            .filter(([, value]) => value !== null && value !== undefined)
+            .map(([key, value]) => `${key}=${value}`).join('&');
+    }
+
+    // requestUrl 값으로 POST 요청 객체 생성
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", requestUrl, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // POST 요청 전송
+    sendHttpRequest(xhr, requestBody, ajaxHandler);
+}
+
+/**
+ * Form 전송 시 요소 유효성 검사 후 AJAX 요청을 처리하는 함수
+ * onsubmit 이벤트 핸들러로 사용 (사용 시 이벤트가 취소됨)
+ */
+function ajaxSubmit(event, ajaxHandler) {
     // 기본 이벤트 취소
     event.preventDefault();
 
@@ -82,19 +88,18 @@ function ajaxSubmit(event) {
         }
     }
 
-    // form 요소로부터 requestBody 객체 생성
-    const requestBody = {};
-    for (var input of form.elements) {
-        if (input.name) {
-            requestBody[input.name] = input.value;
-        }
-    }
-
     // submitter 버튼의 formaction 속성 또는 form 요소의 action 속성으로 요청 URL 결정
-    var action = event.submitter.getAttribute("formaction") || form.action;
+    var requestUrl = event.submitter.getAttribute("formaction") || form.action;
 
-    // ajax 요청 전송
-    ajax(action, requestBody);
+    // requestUrl 값으로 POST 요청 객체 생성
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", requestUrl, true);
+
+    // POST 요청 전송
+    sendHttpRequest(
+        xhr,
+        new FormData(form),
+        ajaxHandler);
 }
 
 var defaultAjaxHandler = function (status, response) {
