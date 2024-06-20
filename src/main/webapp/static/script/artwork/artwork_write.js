9e303f1e50f3c105ca2f5eb7ee41f6fff7051879
PresentKim <debe3721@gmail.com>
2024-06-20 15:58:50 +0900

    feat(artwork.js): unknown으로 설정되기 전 값을 기록 후 재사용

--- src/main/webapp/static/script/artwork/artwork_write.js
+++ src/main/webapp/static/script/artwork/artwork_write.js
@@ -1,28 +1,33 @@
+var artworkArtistBackup = "";
+var artworkYearBackup = "";
+
 /**
  * "작자미상" 체크박스의 체크 변경 이벤트 핸들러
- * 체크된 경우 작가명 입력란의 값을 "작자미상"으로 설정하고, 체크 해제된 경우 빈 칸으로 설정한다.
+ * 체크된 경우 작가명 입력란의 값을 "작자미상"으로 설정하고, 체크 해제된 경우 이전 값으로 설정한다.
  */
 function onUnknownArtistChange() {
     let artistInput = document.getElementById("artist");
 
     if (document.getElementById("unknown-artist").checked) {
+        artworkArtistBackup = artistInput.value;
         artistInput.value = "작자미상";
     } else {
-        artistInput.value = "";
+        artistInput.value = artworkArtistBackup;
     }
 }
 
 /**
  * "연도미상" 체크박스의 체크 변경 이벤트 핸들러
- * 체크된 경우 제작연도 입력란의 값을 "연도미상"으로 설정하고, 체크 해제된 경우 빈 칸으로 설정한다.
+ * 체크된 경우 제작연도 입력란의 값을 "연도미상"으로 설정하고, 체크 해제된 경우 이전 값으로 설정한다.
  */
 function onUnknownYearChange() {
     let yearInput = document.getElementById("year");
 
     if (document.getElementById("unknown-year").checked) {
+        artworkYearBackup = yearInput.value;
         yearInput.value = "연도미상";
     } else {
-        yearInput.value = "";
+        yearInput.value = artworkYearBackup;
     }
 }
 
