c10b0f2031ad948dcf3ce3f1f33967394dffd6b4
PresentKim <debe3721@gmail.com>
2024-06-20 14:14:40 +0900

    feat: `artwork/delete`, `artwork/toggleArtworkDisplay` 구현

--- src/main/java/com/team4/artgallery/controller/ArtworkController.java
+++ src/main/java/com/team4/artgallery/controller/ArtworkController.java
@@ -3,17 +3,17 @@ package com.team4.artgallery.controller;
 import com.team4.artgallery.dao.IArtworkDao;
 import com.team4.artgallery.dto.ArtworkDto;
 import com.team4.artgallery.service.ArtworkService;
+import com.team4.artgallery.service.MemberService;
 import com.team4.artgallery.util.Pagination;
 import com.team4.artgallery.util.ajax.ResponseHelper;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/artwork")
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArtworkController {
 
     private final ArtworkService artworkService;
+
+    private final MemberService memberService;
 
     @Delegate
     private final ResponseHelper responseHelper;
@@ -53,9 +55,41 @@ public class ArtworkController {
         return "artwork/artworkUpdateForm";
     }
 
+    @PostMapping("/toggleArtworkDisplay")
+    public ResponseEntity<?> toggleArtworkDisplay(@RequestParam(value = "aseq") int aseq, HttpSession session) {
+        // 관리자가 아닌 경우 요청 거부 결과 반환
+        if (!memberService.isAdmin(session)) {
+            return forbidden();
+        }
+
+        // 전시 여부 변경 실패 시 오류 결과 반환
+        if (artworkService.toggleArtworkDisplay(aseq) != 1) {
+            return badRequest("전시 여부 변경에 실패했습니다.");
+        }
+
+        // 전시 여부 변경 성공 시 성공 결과 반환
+        return ok("전시 여부가 변경되었습니다.");
+    }
+
     @GetMapping("/write")
     public String write() {
         return "artwork/artworkWriteForm";
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam(value = "aseq") int aseq, HttpSession session) {
+        // 관리자가 아닌 경우 요청 거부 결과 반환
+        if (!memberService.isAdmin(session)) {
+            return forbidden();
+        }
+
+        // 작품 삭제 실패 시 오류 결과 반환
+        if (artworkService.deleteArtwork(aseq) != 1) {
+            return badRequest("작품 삭제에 실패했습니다.");
+        }
+
+        // 작품 삭제 성공 시 성공 결과 반환
+        return ok("작품이 삭제되었습니다.", "/artwork");
     }
 
 }
