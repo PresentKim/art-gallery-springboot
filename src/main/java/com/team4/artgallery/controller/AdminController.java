e1606ec13f00b11aafd1956424e450b4945bb6cd
PresentKim <debe3721@gmail.com>
2024-06-24 17:19:32 +0900

    feat: `admin/main` 기능 구현

--- src/main/java/com/team4/artgallery/controller/AdminController.java
+++ src/main/java/com/team4/artgallery/controller/AdminController.java
@@ -1,15 +1,30 @@
 package com.team4.artgallery.controller;
 
+import com.team4.artgallery.service.MemberService;
+import com.team4.artgallery.util.ajax.ResponseHelper;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class AdminController {
 
-    @GetMapping("/")
-    public String root() {
+    private final MemberService memberService;
+
+    @GetMapping({"", "/"})
+    public String root(HttpSession session) {
+        // 관리자가 아닌 경우 404 페이지로 포워딩
+        if (!memberService.isAdmin(session)) {
+            System.out.println("관리자가 아닙니다");
+            return "util/404";
+        }
+
         return "admin/adminMain";
     }
 
