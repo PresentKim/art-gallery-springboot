af684690b8ebdd53da59c210ea4c4b7cc6281f7a
PresentKim <debe3721@gmail.com>
2024-06-25 10:10:39 +0900

    feat: `admin/artwork/delete` 기능 구현

--- src/main/java/com/team4/artgallery/controller/admin/AdminArtworkController.java
+++ src/main/java/com/team4/artgallery/controller/admin/AdminArtworkController.java
@@ -10,12 +10,12 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/artwork")
@@ -48,6 +48,31 @@ public class AdminArtworkController {
         model.addAttribute("pagination", pair.pagination());
         model.addAttribute("artworkList", pair.list());
         return "admin/adminArtworkList";
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(
+            @RequestParam(value = "aseqs", required = false) List<Integer> aseqs,
+            HttpSession session
+    ) {
+        // aseqs 값이 없는 경우 요청 거부 결과 반환
+        if (aseqs == null || aseqs.isEmpty()) {
+            return badRequest("예술품을 선택해주세요");
+        }
+
+        // 관리자가 아닌 경우 요청 거부 결과 반환
+        if (!memberService.isAdmin(session)) {
+            return forbidden();
+        }
+
+        // 예술품 정보 제거에 실패한 경우 실패 결과 반환
+        if (artworkService.deleteArtworks(aseqs) == 0) {
+            return badRequest("예술품 정보 제거에 실패했습니다");
+        }
+
+        // 예술품 정보 제거에 성공한 경우 성공 결과 반환 (페이지 새로고침)
+        // TODO: 새고로침 없이 HTML 요소를 변경하는 방법으로 수정
+        return ok("예술품 정보를 제거했습니다", ":reload");
     }
 
 }
