430ad2c487c71c7405db4cb86c016d622314670a
PresentKim <debe3721@gmail.com>
2024-06-26 06:52:33 +0900

    feat: 정수 파라미터를 Pagination 객체로 변환하는 Converter 클래스 구현

--- src/main/java/com/team4/artgallery/ArtGalleryWebConfig.java
+++ src/main/java/com/team4/artgallery/ArtGalleryWebConfig.java
@@ -1,9 +1,11 @@
 package com.team4.artgallery;
 
+import com.team4.artgallery.converter.PaginationConverter;
 import com.team4.artgallery.resolver.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,10 +16,16 @@ import java.util.List;
 public class ArtGalleryWebConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final PaginationConverter paginationConverter;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(paginationConverter);
     }
 
 }
