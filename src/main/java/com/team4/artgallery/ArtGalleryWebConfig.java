9d5e87892da4bd480a2f9bf77b3440e10db15779
PresentKim <debe3721@gmail.com>
2024-06-25 22:38:17 +0900

    feat: 로그인한 회원 정보를 주입받을 때 사용하는 파라미터 어노테이션 `LoginMember` 추가

---
+++ src/main/java/com/team4/artgallery/ArtGalleryWebConfig.java
@@ -0,0 +1,23 @@
+package com.team4.artgallery;
+
+import com.team4.artgallery.security.LoginMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class ArtGalleryWebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
+}
---
+++ src/main/java/com/team4/artgallery/security/LoginMemberArgumentResolver.java
@@ -0,0 +1,42 @@
+package com.team4.artgallery.security;
+
+import com.team4.artgallery.annotation.LoginMember;
+import com.team4.artgallery.security.exception.NotLoginException;
+import com.team4.artgallery.service.MemberService;
+import com.team4.artgallery.service.SessionService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+    private final SessionService sessionService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginMember.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer, @NonNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // 세션이 없거나 로그인하지 않은 경우 예외 발생
+        HttpSession session = sessionService.getSession();
+        if (session == null || !memberService.isLogin(session)) {
+            throw new NotLoginException();
+        }
+
+        // 로그인한 회원 정보를 반환합니다.
+        return memberService.getLoginMember(session);
+    }
+
+}
