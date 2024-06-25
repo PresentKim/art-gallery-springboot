51654c20cb3571df7b2a88434aa9e600352c7f1d
PresentKim <debe3721@gmail.com>
2024-06-25 11:02:39 +0900

    feat: `admin/gallery/delete` 기능 구현

--- src/main/java/com/team4/artgallery/controller/admin/AdminGalleryController.java
+++ src/main/java/com/team4/artgallery/controller/admin/AdminGalleryController.java
@@ -9,11 +9,15 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/gallery")
@@ -46,6 +50,31 @@ public class AdminGalleryController {
         model.addAttribute("pagination", pair.pagination());
         model.addAttribute("galleryList", pair.list());
         return "admin/adminGalleryList";
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(
+            @RequestParam(value = "gseqs", required = false) List<Integer> gseqs,
+            HttpSession session
+    ) {
+        // gseqs 값이 없는 경우 요청 거부 결과 반환
+        if (gseqs == null || gseqs.isEmpty()) {
+            return badRequest("갤러리를 선택해주세요");
+        }
+
+        // 관리자가 아닌 경우 요청 거부 결과 반환
+        if (!memberService.isAdmin(session)) {
+            return forbidden();
+        }
+
+        // 갤러리 정보 제거에 실패한 경우 실패 결과 반환
+        if (galleryService.deleteGalleries(gseqs) == 0) {
+            return badRequest("갤러리 정보 제거에 실패했습니다");
+        }
+
+        // 갤러리 정보 제거에 성공한 경우 성공 결과 반환 (페이지 새로고침)
+        // TODO: 새고로침 없이 HTML 요소를 변경하는 방법으로 수정
+        return ok("갤러리 정보를 제거했습니다", ":reload");
     }
 
 }
