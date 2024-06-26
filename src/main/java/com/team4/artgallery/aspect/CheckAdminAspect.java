20b03b144801ef9c6e85a29c0e8fad179d30221a
PresentKim <debe3721@gmail.com>
2024-06-27 02:44:00 +0900

    fix: Aspect 로직에서 `annotation`과 `within`을 각각 핸들링 해 처리 메서드로 전달합니다

--- src/main/java/com/team4/artgallery/aspect/CheckAdminAspect.java
+++ src/main/java/com/team4/artgallery/aspect/CheckAdminAspect.java
@@ -18,13 +18,40 @@ public class CheckAdminAspect {
     private final MemberService memberService;
     private final SessionService sessionService;
 
-    @Before(value = "@annotation(checkAdmin) || @within(checkAdmin)")
-    public void checkAdmin(CheckAdmin checkAdmin) {
-        // 어노테이션이 null 인 경우예외 발생
-        if (checkAdmin == null) {
-            throw new NotAdminException();
-        }
 
+    /**
+     * <code>@annotation</code>을 통해 메서드에 붙은 {@link CheckAdmin} 어노테이션을 받아
+     * {@link #checkAdmin(CheckAdmin)} 메서드에 전달합니다.
+     *
+     * @implNote @Before(value = "@annotation(com.team4.artgallery.aspect.annotation.CheckAdmin)")
+     * 위와 같은 방식으로 직접 클래스를 작성해도 되지만, "어노테이션 매개변수" 방식을 통해 사용할 수 있습니다.
+     * 이 경우 해당 어노테이션 매개변수의 이름을 클래스 대신 사용하면 됩니다.
+     * {@see https://docs.spring.io/spring-framework/reference/core/aop/ataspectj/advice.html}
+     */
+    @Before(value = "@annotation(annotation)")
+    public void onAnnotation(CheckAdmin annotation) {
+        checkAdmin(annotation);
+    }
+
+    /**
+     * <code>@within</code>을 통해 클래스에 붙은 {@link CheckAdmin} 어노테이션을 받아
+     * {@link #checkAdmin(CheckAdmin)} 메서드에 전달합니다.
+     *
+     * @implNote @within() 은 클래스에 붙은 어노테이션을 찾아내는데 사용됩니다.
+     */
+    @Before(value = "@within(annotation)")
+    public void onWithin(CheckAdmin annotation) {
+        checkAdmin(annotation);
+    }
+
+    /**
+     * {@link CheckAdmin} 어노테이션을 처리하는 메서드입니다.
+     * <p>
+     * 세션이 없거나 관리자가 아닌 경우 {@link NotAdminException} 예외를 발생시킵니다.
+     *
+     * @param annotation CheckAdmin 어노테이션
+     */
+    private void checkAdmin(CheckAdmin annotation) {
         // 세션이 없거나 관리자가 아닌 경우 예외 발생
         HttpSession session = sessionService.getSession();
         if (session == null || !memberService.isAdmin(session)) {
--- src/main/java/com/team4/artgallery/aspect/CheckLoginAspect.java
+++ src/main/java/com/team4/artgallery/aspect/CheckLoginAspect.java
@@ -20,21 +20,40 @@ public class CheckLoginAspect {
     private final MemberService memberService;
     private final SessionService sessionService;
 
-    /*
-     * @Before(value = "@annotation(com.team4.artgallery.aspect.annotation.CheckAdmin)")
-     * 위와 같은 방식으로 직접 클래스를 작성해도 되지만, "어노테이션 매개변수" 방식을 통해 사용할 수 있습니다.
-     * 이 경우 해당 어노테이션 매개변수의 이름을 클래스 대신 사용하면 됩니다. (pointcut expression)
+    /**
+     * <code>@annotation</code>을 통해 메서드에 붙은 {@link CheckLogin} 어노테이션을 받아
+     * {@link #checkLogin(JoinPoint, CheckLogin)} 메서드에 전달합니다.
      *
-     * @annotation() 은 메서드에 붙은 어노테이션을 찾아내는데 사용됩니다.
-     * @within() 은 클래스에 붙은 어노테이션을 찾아내는데 사용됩니다.
+     * @implNote @Before(value = "@annotation(com.team4.artgallery.aspect.annotation.CheckAdmin)")
+     * 위와 같은 방식으로 직접 클래스를 작성해도 되지만, "어노테이션 매개변수" 방식을 통해 사용할 수 있습니다.
+     * 이 경우 해당 어노테이션 매개변수의 이름을 클래스 대신 사용하면 됩니다.
+     * {@see https://docs.spring.io/spring-framework/reference/core/aop/ataspectj/advice.html}
      */
-    @Before(value = "@annotation(checkLogin) || @within(checkLogin)")
-    public void checkLogin(JoinPoint joinPoint, CheckLogin checkLogin) {
-        // 어노테이션이 null 인 경우예외 발생
-        if (checkLogin == null) {
-            throw new NotLoginException();
-        }
+    @Before(value = "@annotation(annotation)")
+    public void onAnnotation(JoinPoint joinPoint, CheckLogin annotation) {
+        checkLogin(joinPoint, annotation);
+    }
 
+    /**
+     * <code>@within</code>을 통해 클래스에 붙은 {@link CheckLogin} 어노테이션을 받아
+     * {@link #checkLogin(JoinPoint, CheckLogin)} 메서드에 전달합니다.
+     *
+     * @implNote @within() 은 클래스에 붙은 어노테이션을 찾아내는데 사용됩니다.
+     */
+    @Before(value = "@within(annotation)")
+    public void onWithin(JoinPoint joinPoint, CheckLogin annotation) {
+        checkLogin(joinPoint, annotation);
+    }
+
+    /**
+     * CheckLogin 어노테이션을 처리하는 메서드입니다.
+     * <p>
+     * 세션이 없거나 로그인하지 않은 경우 {@link NotLoginException} 예외를 발생시킵니다.
+     *
+     * @param joinPoint  조인 포인트
+     * @param annotation CheckLogin 어노테이션
+     */
+    private void checkLogin(JoinPoint joinPoint, CheckLogin annotation) {
         // 세션이 없거나 로그인하지 않은 경우 예외 발생
         HttpSession session = sessionService.getSession();
         if (session == null || !memberService.isLogin(session)) {
@@ -45,10 +64,11 @@ public class CheckLoginAspect {
 
             // CheckLogin 어노테이션 객체의 returnUrl 값을 가져와 ${파라미터 이름} 형식으로 작성된 파라미터 값으로 치환합니다.
             // ex) /gallery/update?gseq=${gseq} -> /gallery/update?gseq=1
-            String returnUrl = checkLogin.value();
+            String returnUrl = annotation.value();
             for (int i = 0; i < paramNames.length; i++) {
-                if (returnUrl.contains("${" + paramNames[i] + "}")) {
-                    returnUrl = returnUrl.replace("${" + paramNames[i] + "}", args[i].toString());
+                String replacement = "${" + paramNames[i] + "}";
+                if (returnUrl.contains(replacement)) {
+                    returnUrl = returnUrl.replace(replacement, args[i].toString());
                 }
             }
 
