8c68aa9bf7ec7e846da80c3354984453de920ef2
PresentKim <debe3721@gmail.com>
2024-06-27 11:40:28 +0900

    refactor: 불필요한 PaginationConverter 클래스 제거

--- src/main/java/com/team4/artgallery/ArtGalleryWebConfig.java
+++ src/main/java/com/team4/artgallery/ArtGalleryWebConfig.java
@@ -1,10 +1,8 @@
 package com.team4.artgallery;
 
 import com.team4.artgallery.controller.resolver.LoginMemberArgumentResolver;
-import com.team4.artgallery.util.PaginationConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,16 +13,10 @@ import java.util.List;
 public class ArtGalleryWebConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
-    private final PaginationConverter paginationConverter;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(paginationConverter);
     }
 
 }
