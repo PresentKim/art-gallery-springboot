911083e7b6dc6cf284374b190a7aaba3dd89967a
PresentKim <debe3721@gmail.com>
2024-06-28 12:27:17 +0900

    refactor(AdminController): 오류 핸들링을 `GlobalExceptionHandler`에서 처리하도록 수정

--- src/main/java/com/team4/artgallery/controller/domain/admin/AdminController.java
+++ src/main/java/com/team4/artgallery/controller/domain/admin/AdminController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-
 @Controller
 @RequestMapping("/admin")
 @CheckAdmin
@@ -30,15 +28,9 @@ public class AdminController {
     }
 
     @PostMapping("/reset")
-    public ResponseEntity<?> reset() {
-        // 데이터베이스 초기화 시도
-        try {
-            adminService.resetDatabase();
-        } catch (Exception e) {
-            // 오류가 발생한 경우 실패 응답 반환
-            System.out.println(Arrays.toString(e.getStackTrace()));
-            return internalServerError("데이터베이스 초기화에 실패했습니다.");
-        }
+    public ResponseEntity<?> reset() throws Exception {
+        // 데이터베이스 초기화
+        adminService.resetDatabase();
 
         // 데이터베이스 초기화에 성공한 경우 성공 응답 반환
         return ok("데이터베이스가 초기화되었습니다.");
