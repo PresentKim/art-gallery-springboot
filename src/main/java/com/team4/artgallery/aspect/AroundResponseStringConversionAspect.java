a1f0d0da8acb2a73ef53bce7b5a74e2bb9319d47
PresentKim <debe3721@gmail.com>
2024-07-04 14:40:55 +0900

    fix(AroundResponseStringConversionAspect): `RestController` 어노테이션을 처리하도록 수정

--- src/main/java/com/team4/artgallery/aspect/AroundResponseStringConversionAspect.java
+++ src/main/java/com/team4/artgallery/aspect/AroundResponseStringConversionAspect.java
@@ -8,18 +8,19 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 /**
- * REST 메소드의 응답을 {@link ResponseBody} 객체로 감싸 반환하는 Aspect 클래스
+ * {@link RestController} 어노테이션이 달린 클래스의 메소드 혹은 {@link ExceptionHandler} 어노테이션이 달린 메소드의
+ * 응답을 {@link ResponseBody} 객체로 감싸 반환하는 Aspect 클래스
  *
- * @apiNote {@link RestController} 어노테이션이 달린 메소드가 호출될 때 반환 값을 변환해 반환합니다.
+ * @apiNote 조건에 해당하는 메소드가 호출될 때 반환 값을 아래와 같이 변환해 반환합니다.
  * <ul>
  *     <li>{@code String} : 반환 값을 {@code message}값으로 {@link ResponseBody} 객체를 생성해 반환</li>
  *     <li>{@code URI} : 반환 값을 {@code url}값으로 {@link ResponseBody} 객체를 생성해 반환</li>
+ *     <li>그 외의 값 : 그대로 반환</li>
  * </ul>
  */
 @Aspect
@@ -28,16 +29,18 @@ import java.net.URI;
 public class AroundResponseStringConversionAspect {
 
     /**
-     * {@link RequestMapping} 혹은 {@link ExceptionHandler} 어노테이션이 달린 메소드가 문자열을 반환하면 {@link ResponseBody} 객체로 감싸 반환합니다.
+     * {@link RestController} 어노테이션이 달린  클래스의 메소드 혹은 {@link ExceptionHandler} 어노테이션이 달린 메소드가
+     * 문자열을 반환하면 {@link ResponseBody} 객체로 감싸 반환합니다.
      *
      * @implNote {@link Around} 어노테이션으로 메서드 실행 전후에 로직을 추가할 수 있습니다.
+     * <p>
+     * {@code value}값의 {@code @within} 은 클래스에 붙은 어노테이션을 찾아내는데 사용됩니다.
      * <p>
      * {@code value}값의 {@code @annotation}은 메서드에 붙은 어노테이션을 찾아내는데 사용됩니다.
      * <p>
      * {@link ProceedingJoinPoint} 객체는 메서드의 정보를 담고 있습니다. 이를 통해 메서드의 실행을 직접 제어할 수 있습니다.
      */
-    @Around("@annotation(org.springframework.web.bind.annotation.GetMapping) || " +
-            "@annotation(org.springframework.web.bind.annotation.PostMapping) || " +
+    @Around("@within(org.springframework.web.bind.annotation.RestController) || " +
             "@annotation(org.springframework.web.bind.annotation.ExceptionHandler)")
     public Object onAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
         // 메서드 실행
