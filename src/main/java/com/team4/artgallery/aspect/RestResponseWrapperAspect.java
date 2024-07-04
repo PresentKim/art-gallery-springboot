d6cbda928ca01889a3c8c1202d867e04f3d4aea7
PresentKim <debe3721@gmail.com>
2024-07-04 15:09:03 +0900

    fix(RestResponseWrapperAspect): `ResponseBody` 어노테이션을 처리하도록 수정

--- src/main/java/com/team4/artgallery/aspect/RestResponseWrapperAspect.java
+++ src/main/java/com/team4/artgallery/aspect/RestResponseWrapperAspect.java
@@ -8,13 +8,15 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 /**
- * {@link RestController} 어노테이션이 달린 클래스의 메소드 혹은 {@link ExceptionHandler} 어노테이션이 달린 메소드의
- * 응답을 {@link ResponseDto} 객체로 감싸 반환하는 Aspect 클래스
+ * {@link RestController} 어노테이션이 달린 클래스의 메소드,
+ * {@link ResponseBody} 어노테이션이 달린 메소드의 응답,
+ * {@link ExceptionHandler} 어노테이션이 달린 메소드의 응답을 {@link ResponseDto} 객체로 감싸 반환하는 Aspect 클래스
  *
  * @apiNote 조건에 해당하는 메소드가 호출될 때 반환 값을 아래와 같이 변환해 반환합니다.
  * <ul>
@@ -29,8 +31,9 @@ import java.net.URI;
 public class RestResponseWrapperAspect {
 
     /**
-     * {@link RestController} 어노테이션이 달린 클래스의 메소드 혹은 {@link ExceptionHandler} 어노테이션이 달린 메소드가
-     * 문자열을 반환하면 {@link ResponseDto} 객체로 감싸 반환합니다.
+     * {@link RestController} 어노테이션이 달린 클래스의 메소드,
+     * {@link ResponseBody} 어노테이션이 달린 메소드의 응답,
+     * {@link ExceptionHandler} 어노테이션이 달린 메소드의 응답을 {@link ResponseDto} 객체로 감싸 반환합니다.
      *
      * @implNote {@link Around} 어노테이션으로 메서드 실행 전후에 로직을 추가할 수 있습니다.
      * <p>
@@ -41,6 +44,7 @@ public class RestResponseWrapperAspect {
      * {@link ProceedingJoinPoint} 객체는 메서드의 정보를 담고 있습니다. 이를 통해 메서드의 실행을 직접 제어할 수 있습니다.
      */
     @Around("@within(org.springframework.web.bind.annotation.RestController) || " +
+            "@annotation(org.springframework.web.bind.annotation.ResponseBody) || " +
             "@annotation(org.springframework.web.bind.annotation.ExceptionHandler)")
     public Object onAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
         // 메서드 실행
