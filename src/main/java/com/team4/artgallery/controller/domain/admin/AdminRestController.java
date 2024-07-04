1602ce81115bf7949d33e31a8321f3bd27b07260
PresentKim <debe3721@gmail.com>
2024-07-04 09:51:07 +0900

    refactor(AdminRestController): `ResponseEntity` 대신 문자열로 메시지를 응답

--- src/main/java/com/team4/artgallery/controller/domain/admin/AdminRestController.java
+++ src/main/java/com/team4/artgallery/controller/domain/admin/AdminRestController.java
@@ -2,13 +2,12 @@ package com.team4.artgallery.controller.domain.admin;
 
 import com.team4.artgallery.aspect.annotation.CheckAdmin;
 import com.team4.artgallery.service.AdminService;
-import com.team4.artgallery.service.helper.ResponseService;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.Delegate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,16 +18,14 @@ public class AdminRestController {
 
     private final AdminService adminService;
 
-    @Delegate
-    private final ResponseService responseHelper;
-
     @PostMapping("/reset")
-    public ResponseEntity<?> reset() throws Exception {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Object reset() throws Exception {
         // 데이터베이스 초기화
         adminService.resetDatabase();
 
         // 데이터베이스 초기화에 성공한 경우 성공 응답 반환
-        return ok("데이터베이스가 초기화되었습니다.");
+        return "데이터베이스가 초기화되었습니다.";
     }
 
 }
