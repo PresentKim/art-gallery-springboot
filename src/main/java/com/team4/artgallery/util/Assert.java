8604849c5cf55af906821864e33e4e62e499c624
PresentKim <debe3721@gmail.com>
2024-07-04 12:33:20 +0900

    refactor(Assert): 사용되지 않는 메서드 제거

--- src/main/java/com/team4/artgallery/util/Assert.java
+++ src/main/java/com/team4/artgallery/util/Assert.java
@@ -69,36 +69,6 @@ public class Assert {
     }
 
     /**
-     * 값이 0인 경우 주어진 예외를 발생시킵니다.
-     *
-     * @param message 예외 메시지
-     * @throws T 값이 0인 경우 예외 발생
-     */
-    public <T extends Throwable> void notZero(int value, String message, Function<String, T> throwableConstructor) throws T {
-        isFalse(value == 0, message, throwableConstructor);
-    }
-
-    /**
-     * 값이 null이 아닌 경우 주어진 예외를 발생시킵니다.
-     *
-     * @param message 예외 메시지
-     * @throws T 값이 null이 아닌 경우 예외 발생
-     */
-    public <T extends Throwable> void isNull(Object value, String message, Function<String, T> throwableConstructor) throws T {
-        isTrue(value == null, message, throwableConstructor);
-    }
-
-    /**
-     * 값이 null인 경우 주어진 예외를 발생시킵니다.
-     *
-     * @param message 예외 메시지
-     * @throws T 값이 null인 경우 예외 발생
-     */
-    public <T extends Throwable> void notNull(Object value, String message, Function<String, T> throwableConstructor) throws T {
-        isFalse(value == null, message, throwableConstructor);
-    }
-
-    /**
      * 값이 비어있는 경우 주어진 예외를 발생시킵니다.
      *
      * @param message 예외 메시지
@@ -106,16 +76,6 @@ public class Assert {
      */
     public <T extends Throwable> void notEmpty(Object value, String message, Function<String, T> throwableConstructor) throws T {
         isTrue(getSize(value) == 0, message, throwableConstructor);
-    }
-
-    /**
-     * 값의 길이가 1이 아닌 경우 주어진 예외를 발생시킵니다.
-     *
-     * @param message 예외 메시지
-     * @throws T 값의 길이가 1이 아닌 경우 예외 발생
-     */
-    public <T extends Throwable> void isSingle(Object value, String message, Function<String, T> throwableConstructor) throws T {
-        isTrue(getSize(value) == 1, message, throwableConstructor);
     }
 
     /**
