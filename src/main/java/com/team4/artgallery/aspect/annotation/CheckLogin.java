42af02ea7bda6f44d75323f9d9bc318184520cef
PresentKim <debe3721@gmail.com>
2024-07-05 13:29:01 +0900

    fix: `@AliasFor` 대신 주석으로 `value` 필드에 대해 설명하도록 수정
    @AliasFor 어노테이션은 아무래도 스프링 프레임워크 내부적으로 구현된 어노테이션에서만 작동하는 것 같습니다.
    실제로 실행해보면 다른 필드의 값은 변경되지 않고 있기 때문에 사용하지 않도록 수정합니다.

--- src/main/java/com/team4/artgallery/aspect/annotation/CheckLogin.java
+++ src/main/java/com/team4/artgallery/aspect/annotation/CheckLogin.java
@@ -3,7 +3,6 @@ package com.team4.artgallery.aspect.annotation;
 import com.team4.artgallery.aspect.CheckLoginAspect;
 import com.team4.artgallery.aspect.exception.NotLoginException;
 import com.team4.artgallery.controller.advice.GlobalExceptionHandler;
-import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -20,7 +19,7 @@ import java.lang.annotation.Target;
  * public String update()
  * </pre></blockquote>
  * <p>
- * 어노테이션의 {@link #returnUrl} 또는 {@link #value}를 설정해 이동시킬 URL을 지정할 수 있고, 설정하지 않으면 페이지 이동을 하지 않습니다.
+ * 어노테이션의 {@link #value}를 설정해 이동시킬 URL을 지정할 수 있고, 설정하지 않으면 페이지 이동을 하지 않습니다.
  * 템플릿 문자열처럼 {@code ${매개변수명}} 표현식을 사용할 수 있습니다. 메서드에 존재하는 매개변수 중 해당 이름의 값을 사용합니다.
  * <p>
  * 클래스 또는 메서드에 사용할 수 있습니다.
@@ -28,7 +27,7 @@ import java.lang.annotation.Target;
  * @implNote 관점 지향 프로그래밍(AOP)을 위해 반복 사용되는 로그인 여부 확인을 어노테이션으로 처리합니다.
  * <p>
  * 로그인 여부 확인은 {@link CheckLoginAspect}에서 처리하며, 로그인하지 않은 경우 {@link NotLoginException} 예외가 발생합니다.
- * {@link NotLoginException#getReturnUrl}에는 {@link CheckLogin}의 {@link #returnUrl} 또는 {@link #value}로 설정한 URL이 저장됩니다.
+ * {@link NotLoginException#getReturnUrl}에는 {@link CheckLogin}의 {@link #value}로 설정한 URL이 저장됩니다.
  * <p>
  * 최종적으로 예외 핸들링 및 응답 처리는 {@link GlobalExceptionHandler#handleNotLoginException(NotLoginException)}에서 처리됩니다.
  * <p>
@@ -36,9 +35,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface CheckLogin {
-    @AliasFor("value")
-    String returnUrl() default "";
-
-    @AliasFor("returnUrl")
+    /**
+     * {@code returnUrl} 이동시킬 URL
+     */
     String value() default "";
 }
--- src/main/java/com/team4/artgallery/aspect/annotation/QueryApplied.java
+++ src/main/java/com/team4/artgallery/aspect/annotation/QueryApplied.java
@@ -3,7 +3,6 @@ package com.team4.artgallery.aspect.annotation;
 import com.team4.artgallery.aspect.QueryAppliedAspect;
 import com.team4.artgallery.controller.advice.GlobalExceptionHandler;
 import com.team4.artgallery.controller.exception.SqlException;
-import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.lang.annotation.ElementType;
@@ -20,14 +19,14 @@ import java.lang.annotation.Target;
  * int createArtwork(ArtworkDto artworkDto);
  * </pre></blockquote>
  * <p>
- * 어노테이션의 {@link #message} 또는 {@link #value}를 설정해 메시지를 지정할 수 있고, 설정하지 않으면 기본 메시지를 사용합니다.
+ * 어노테이션의 {@link #value}를 설정해 메시지를 지정할 수 있고, 설정하지 않으면 기본 메시지를 사용합니다.
  * <p>
  * 메서드에 사용할 수 있습니다.
  * <p>
  * @implNote 관점 지향 프로그래밍(AOP)을 위해 반복 사용되는 쿼리 결과 확인을 어노테이션으로 처리합니다.
  * <p>
  * 쿼리 결과 확인은 {@link QueryAppliedAspect}에서 처리하며, 쿼리 결과 값이 0인 경우 {@link SqlException} 예외가 발생합니다.
- * {@link SqlException#reason}에는 {@link QueryApplied}의 {@link #message} 또는 {@link #value}로 설정한 URL이 저장됩니다.
+ * {@link SqlException#reason}에는 {@link QueryApplied}의 또는 {@link #value}로 설정한 문자열이 저장됩니다.
  * <p>
  * 최종적으로 예외 핸들링 및 응답 처리는 {@link GlobalExceptionHandler#handleResponseStatusException(ResponseStatusException)}에서 처리됩니다.
  * <p>
@@ -35,9 +34,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface QueryApplied {
-    @AliasFor("value")
-    String message() default "오류가 발생했습니다.";
-
-    @AliasFor("message")
+    /**
+     * {@code message} 오류 메시지
+     */
     String value() default "오류가 발생했습니다.";
 }
