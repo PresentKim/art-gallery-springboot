c08a440ee14eee3571891f222cd925d11ebe0365
PresentKim(김현재) <debe3721@gmail.com>
2024-07-08 16:28:04 +0900

    refactor: AJAX 처리 로직을 Promise.then() 대신 async-await 구조로 변경

--- src/main/webapp/static/script/artwork/artwork_view.js
+++ src/main/webapp/static/script/artwork/artwork_view.js
@@ -1,7 +1,9 @@
-function toggleArtworkDisplay(element, aseq) {
-    ajax('/artwork/toggleArtworkDisplay', {aseq}, 'POST')
-        .then(defaultAjaxHandler)
-        .then(() => {
-            element.textContent = element.textContent.includes('비') ? '공개로 전환' : '비공개로 전환';
-        });
+async function toggleArtworkDisplay(element, aseq) {
+    const response = await ajax('/artwork/toggleArtworkDisplay', {aseq}, 'POST');
+
+    // element의 텍스트를 '비'를 포함하고 있으면 '공개로 전환', 그렇지 않으면 '비공개로 전환'으로 설정
+    element.textContent = element.textContent.includes('비') ? '공개로 전환' : '비공개로 전환';
+
+    // 메시지 출력은 defaultAjaxHandler 에서 처리
+    defaultAjaxHandler(response);
 }
--- src/main/webapp/static/script/qna.js
+++ src/main/webapp/static/script/qna.js
@@ -1,15 +1,19 @@
-function qnaAuth(qseq, mode, pwd) {
-    ajax('/qna/authorize', {qseq, mode, pwd}, 'POST')
-        .then((response) => {
-            if (response.status === 401) {
-                const pwd = prompt(qseq + "번 QnA 글의 비밀번호를 입력하세요:");
-                if (pwd !== null && pwd !== undefined && pwd !== "") {
-                    qnaAuth(qseq, mode, pwd);
-                } else {
-                    alert("비밀번호 입력이 취소되었습니다.");
-                }
-            } else {
-                defaultAjaxHandler(response);
-            }
-        });
+async function qnaAuth(qseq, mode, pwd) {
+    const response = await ajax('/qna/authorize', {qseq, mode, pwd}, 'POST');
+
+    // 결과 상태 코드가 401이 아니면 defaultAjaxHandler 를 통해 메시지 출력
+    if (response.status !== 401) {
+        defaultAjaxHandler(response);
+        return;
+    }
+
+    // 비밀번호가 입력되지 않았거나, 취소되었을 경우 중단
+    pwd = prompt(`${qseq}번 QnA 글의 비밀번호를 입력하세요:`);
+    if (pwd === null || pwd === undefined || pwd === "") {
+        alert("비밀번호 입력이 취소되었습니다.");
+        return;
+    }
+
+    // 비밀번호를 입력하고 다시 인증 시도
+    await qnaAuth(qseq, mode, pwd);
 }
