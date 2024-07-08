6b36018771afea20d18377baf03c052a8b362b1d
PresentKim(김현재) <debe3721@gmail.com>
2024-07-08 13:18:33 +0900

    refactor(admin): 관리자 페이지의 VIEW 컨트롤러와 REST 컨트롤러를 분리 및 통합

--- src/main/java/com/team4/artgallery/controller/domain/admin/AdminRestController.java
+++ src/main/java/com/team4/artgallery/controller/domain/admin/AdminRestController.java
@@ -5,28 +5,24 @@ import com.team4.artgallery.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 @CheckAdmin
 @RequiredArgsConstructor
-public class AdminController {
+public class AdminRestController {
 
     private final AdminService adminService;
 
-    @GetMapping({"", "/"})
-    public String root() {
-        return "admin/adminMain";
-    }
-
-    @PostMapping(path = "/reset", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "reset", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
     public Object reset() throws SQLException, IOException {
         adminService.resetDatabase();
         return "데이터베이스가 초기화되었습니다.";
---
+++ src/main/java/com/team4/artgallery/controller/domain/admin/AdminViewController.java
@@ -0,0 +1,116 @@
+package com.team4.artgallery.controller.domain.admin;
+
+import com.team4.artgallery.aspect.annotation.CheckAdmin;
+import com.team4.artgallery.controller.exception.SqlException;
+import com.team4.artgallery.dto.filter.ArtworkFilter;
+import com.team4.artgallery.dto.filter.KeywordFilter;
+import com.team4.artgallery.dto.filter.NoticeFilter;
+import com.team4.artgallery.dto.filter.QnaFilter;
+import com.team4.artgallery.service.*;
+import com.team4.artgallery.util.Pagination;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+@CheckAdmin
+@RequiredArgsConstructor
+public class AdminViewController {
+
+    private final ArtworkService artworkService;
+    private final GalleryService galleryService;
+    private final MemberService memberService;
+    private final NoticeService noticeService;
+    private final QnaService qnaService;
+
+    @GetMapping({"", "/"})
+    public String root() {
+        return "admin/adminMain";
+    }
+
+    @GetMapping({"artwork", "artwork/"})
+    public String artwork(
+            @Valid
+            @ModelAttribute("filter")
+            ArtworkFilter filter,
+            @Valid
+            @ModelAttribute("pagination")
+            Pagination pagination,
+
+            Model model
+    ) {
+        model.addAttribute("artworkList", artworkService.getArtworks(filter, pagination));
+        return "admin/adminArtworkList";
+    }
+
+    @GetMapping({"gallery", "gallery/"})
+    public String gallery(
+            @Valid
+            @ModelAttribute("filter")
+            KeywordFilter filter,
+            @Valid
+            @ModelAttribute("pagination")
+            Pagination pagination,
+
+            Model model
+    ) {
+        model.addAttribute("galleryList", galleryService.getGalleries(filter, pagination));
+        return "admin/adminGalleryList";
+    }
+
+    @GetMapping({"member", "member/"})
+    public String member(
+            @Valid
+            @ModelAttribute("filter")
+            KeywordFilter filter,
+            @Valid
+            @ModelAttribute("pagination")
+            Pagination pagination,
+
+            Model model
+    ) {
+        model.addAttribute("memberList", memberService.getMembers(
+                filter,
+                pagination
+                        .setUrlTemplateFromFilter(filter)
+                        .setItemCount(memberService.countMembers(filter))
+        ));
+        return "admin/adminMemberList";
+    }
+
+    @GetMapping({"notice", "notice/"})
+    public String notice(
+            @Valid
+            @ModelAttribute("filter")
+            NoticeFilter filter,
+            @Valid
+            @ModelAttribute("pagination")
+            Pagination pagination,
+
+            Model model
+    ) {
+        model.addAttribute("noticeList", noticeService.getNotices(filter, pagination));
+        return "admin/adminNoticeList";
+    }
+
+    @GetMapping({"qna", "qna/"})
+    public String qna(
+            @Valid
+            @ModelAttribute("filter")
+            QnaFilter filter,
+            @Valid
+            @ModelAttribute("pagination")
+            Pagination pagination,
+
+            Model model
+    ) throws SqlException {
+        model.addAttribute("qnaList", qnaService.getInquiries(filter, pagination));
+        return "admin/adminQnaList";
+    }
+
+}
