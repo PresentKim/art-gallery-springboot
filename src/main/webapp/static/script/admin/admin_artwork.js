f492b514b1d84b603c8c07474af126ba928e7fd9
PresentKim(김현재) <debe3721@gmail.com>
2024-07-11 03:55:05 +0900

    feat(artwork): 예술품 관리자 페이지에 삭제 애니메이션 추가

--- src/main/webapp/static/script/admin/admin_artwork.js
+++ src/main/webapp/static/script/admin/admin_artwork.js
@@ -1,10 +1,17 @@
 function deleteSelectedArtworks() {
+    function deleteAnimate($ul) {
+        $ul.classList.add('deleted');
+        setTimeout(() => $ul.remove(), 500);
+    }
+
     document.querySelectorAll('.check-box:checked').forEach($checkbox => {
         const $ul = $checkbox.parentElement.parentElement.parentElement;
+        $ul.classList.add('delete-target');
 
         axios.delete('/api/artworks/' + $checkbox.value)
-            .then(() => $ul.remove())
+            .then(() => deleteAnimate($ul))
             .catch(error => {
+                $ul.classList.remove('delete-target');
                 defaultAjaxHandler(error.response);
             });
     });
