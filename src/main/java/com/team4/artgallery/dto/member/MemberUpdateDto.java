e5ed88982026264daf3ea8e9cdc414569a5bd0b9
PresentKim(김현재) <debe3721@gmail.com>
2024-07-30 16:10:37 +0900

    fix(MemberUpdateDto): 빈 비밀번호 요청을 허용하기 위해 `Size` 대신 `Pattern` 어노테이션 사용

--- src/main/java/com/team4/artgallery/dto/member/MemberUpdateDto.java
+++ src/main/java/com/team4/artgallery/dto/member/MemberUpdateDto.java
@@ -1,13 +1,14 @@
 package com.team4.artgallery.dto.member;
 
 import com.team4.artgallery.entity.MemberEntity;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record MemberUpdateDto(
         @Size(min = 2, max = 45, message = "이름은 2자 이상 45자 이하로 입력해주세요.")
         String name,
 
-        @Size(min = 4, max = 45, message = "비밀번호는 4자 이상 45자 이하로 입력해주세요.")
+        @Pattern(regexp = "^$|^.{4,45}$", message = "비밀번호는 4자 이상 45자 이하로 입력해주세요.")
         String pwd,
 
         @Size(min = 4, max = 45, message = "이메일은 4자 이상 45자 이하로 입력해주세요.")
